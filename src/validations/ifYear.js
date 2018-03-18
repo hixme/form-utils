3546c603824d62f4cc73313a942c7c6db9b36cd6
David Cotelessa <David@hix00134.attlocal.net>
2018-03-18 16:57:20 -0700

    master: update ifYear to include range

--- src/validations/ifYear.js
+++ src/validations/ifYear.js
@@ -1,9 +1,9 @@
 import moment from 'moment'
 
-export default (year = '') => {
+export default (year, range = 0) => {
   const currentYear = year || Number(moment(Date.now()).format('YYYY'))
   return (str) => {
     const num = Number(str)
-    return (!Number.isNaN(num) && (num === currentYear || num === currentYear + 1)) || false
+    return (!Number.isNaN(num) && (num === currentYear || num === currentYear + range)) || false
   }
 }
