7191c895ad8c8e1090bd558f18e144c987e2be51
David Cotelessa <David@HIX00134.local>
2018-03-22 14:10:54 -0700

    master: ifValidTxt must handle undefined

--- src/functions/ifValidText.js
+++ src/functions/ifValidText.js
@@ -1,2 +1,2 @@
-export default (txt, valid) => (value, allValues, props, name) =>
+export default (txt, valid) => (value = true, allValues, props, name) =>
   (valid.reduce((prev, v) => prev && v(prev, allValues, props, name), value) && txt) || undefined
