37d4d6638596b595343b1b9fea640847a099fd67
David Cotelessa <David@HIX00134.local>
2018-08-08 11:26:00 -0700

    updated diff

--- src/functions/diff.js
+++ src/functions/diff.js
@@ -2,16 +2,23 @@ import { transform, isEqual, isObject } from 'lodash/fp'
 
 const tf = transform.convert({ cap: false })
 
-const diff = (bObj, targetObj) => {
-  const iteratee = baseObj => (result = {}, value, key) => {
-    const newVals = {}
-    if (!isEqual(value, baseObj[key])) {
-      newVals[key] =
-        (isObject(value) && isObject(baseObj[key]) && diff(baseObj[key], value)) || value
+const diff = (baseObj, targetObj) => {
+  const iteratee = (result = {}, value, key) => {
+    const baseObjKeyValue = baseObj[key]
+    if (isEqual(value, baseObjKeyValue)) {
+      return result
     }
+    const newVals = {
+      [key]:
+        (isObject(value) &&
+          isObject(baseObjKeyValue) &&
+          diff(baseObjKeyValue, value)) ||
+        value,
+    }
+
     return Object.assign(result, newVals)
   }
-  return tf(iteratee(bObj), null, targetObj)
+  return tf(iteratee, null, targetObj)
 }
 
 export default diff
